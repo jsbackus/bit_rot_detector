5babb4d0cc0b6578cd7225ac0f02407f70534c47
Jeff Backus <jeff.backus@gmail.com>
2013-10-21 16:50:56 -0400

    * Testing list subcommand with multiple targets complete.

--- unit_tests/test_list.py
+++ unit_tests/test_list.py
@@ -47,7 +47,7 @@ class TestList(BrdUnitBase):
         self.assertEqual( scr_out, exp_out )
 
     def test_file_target(self):
-        """Tests list subcommand with one directory target.
+        """Tests list subcommand with one file target.
         """
 
         mod_time = int(time.time())
@@ -70,6 +70,37 @@ class TestList(BrdUnitBase):
         # Attempt to list contents
         scr_out = subprocess.check_output([self.script_name, 'list', 
                                            'rootA/BunchOfCs.txt'])
+        # Verify output
+        self.assertEqual( scr_out, exp_out )
+
+    def test_multiple_targets(self):
+        """Tests list subcommand with multiple targets.
+        """
+
+        mod_time = int(time.time())
+        exp_out = os.linesep.join( ( 'Files matching rootA/BunchOfCs.txt:', '',
+                                     'BunchOfCs.txt:', '    ID: 1',
+                                     '    Last Modified: ' + str(
+                    datetime.datetime.fromtimestamp(mod_time) ),
+                                     '    Fingerprint: 0xff3785f53b503b7adb7e' +
+                                     '7a3b9eeef255eac0e276',
+                                     '    Size: 257 bytes', '',
+                                     '1 entries listed.', '', 
+                                     'rootA/LeafB:', '    BunchOfAs.txt', 
+                                     '    BunchOfBs.txt', '', 
+                                     '2 entries listed.', '', '' ) )
+
+        # Call open_db, which should create db and its tables
+        self.open_db( self.default_db, False )
+
+        # Populate the database with schema 1.
+        self.populate_db_from_tree( self.get_schema_1( mod_time ) )
+        self.conn.close()
+
+        # Attempt to list contents
+        scr_out = subprocess.check_output([self.script_name, 'list', 
+                                           'rootA/BunchOfCs.txt', 
+                                           'rootA/LeafB'])
         # Verify output
         self.assertEqual( scr_out, exp_out )
 
