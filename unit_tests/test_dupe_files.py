c409fb439cbe44d6a2214184fb95b4d23f4bc20e
Jeff Backus <jeff.backus@gmail.com>
2013-12-01 17:29:11 -0500

    * Removed some commented code from test_dupe_files.py. * Added and debugged test_output_option() to test_dupe_files.py. Closes issue #62.

--- unit_tests/test_dupe_files.py
+++ unit_tests/test_dupe_files.py
@@ -105,6 +105,69 @@ class TestDupeFiles(BrdUnitBase):
         for exp_line in exp_out:
             self.assertTrue( exp_line in scr_lines )
         
+    def test_output_option(self):
+        """Tests dupe_files subcommand with --output option.
+        """
+
+        mod_time = int(time.time())
+        check_time = datetime.datetime.fromtimestamp(mod_time)
+        out_file = 'test_output.txt'
+        exp_out = \
+            ['2 files with Fingerprint ' +
+             '0x1a0372738bb5b4b8360b47c4504a27e6f4811493:\n',
+             '    [rootA]/TreeA/DirA/LeafA/BunchOfAs.txt\n',
+             '    [rootA]/LeafB/BunchOfAs.txt\n',
+             '2 files with Fingerprint ' +
+             '0xfa75bf047f45891daee8f1fa4cd2bf58876770a5:\n',
+             '    [rootA]/TreeA/DirA/LeafA/BunchOfBs.txt\n',
+             '    [rootA]/LeafB/BunchOfBs.txt\n']
+
+        # Call open_db, which should create db and its tables
+        self.open_db( self.default_db, False )
+
+        # Populate the database with schema 1.
+        exp_data = self.get_schema_1( str(mod_time), check_time )
+        self.populate_db_from_tree( exp_data )
+        self.conn.close()
+
+        # Check --output
+        scr_out = subprocess.check_output([self.script_name, 'dupe_files', 
+                                           '--output', out_file], 
+                                          stderr=subprocess.STDOUT,
+                                          universal_newlines=True)
+
+        with open(out_file, 'rt') as f:
+            scr_lines = f.readlines()
+
+        # Verify results
+        self.assertEqual( scr_out, '' )
+
+        self.assertEqual( len(scr_lines), len(exp_out) )
+        for exp_line in exp_out:
+            self.assertTrue( exp_line in scr_lines )
+
+        # Remove output file and try again with -o option
+        os.unlink( out_file )
+
+        # Check -o
+        scr_out = subprocess.check_output([self.script_name, 'dupe_files', 
+                                           '-o', out_file], 
+                                          stderr=subprocess.STDOUT,
+                                          universal_newlines=True)
+
+        with open(out_file, 'rt') as f:
+            scr_lines = f.readlines()
+
+        # Verify results
+        self.assertEqual( scr_out, '' )
+
+        self.assertEqual( len(scr_lines), len(exp_out) )
+        for exp_line in exp_out:
+            self.assertTrue( exp_line in scr_lines )
+
+        # Clean up
+        os.unlink( out_file )
+        
     # def test_identical_files(self):
     #     """Tests dupe_files subcommand with identical files.
     #     """
@@ -188,227 +251,6 @@ class TestDupeFiles(BrdUnitBase):
     #     for exp_line in exp_out:
     #         self.assertTrue( exp_line in scr_lines )
         
-    # def test_output_option(self):
-    #     """Tests dupe_files subcommand with --output option.
-    #     """
-
-    #     mod_time = int(time.time())
-    #     check_time = datetime.datetime.fromtimestamp(mod_time)
-    #     out_file = 'test_output.txt'
-    #     exp_out = ['Only in rootA/LeafB: BunchOfAs.txt\n',
-    #                'Only in rootA/LeafB: BunchOfBs.txt\n',
-    #                'Only in rootA/TreeA: DirA\n']
-
-    #     # Call open_db, which should create db and its tables
-    #     self.open_db( self.default_db, False )
-
-    #     # Populate the database with schema 1.
-    #     exp_data = self.get_schema_1( str(mod_time), check_time )
-    #     self.populate_db_from_tree( exp_data )
-    #     self.conn.close()
-
-    #     # Try --output option
-    #     scr_out = subprocess.check_output([self.script_name, 'dupe_files', 
-    #                                        '--output', out_file,
-    #                                        'rootA/TreeA', 'rootA/LeafB'], 
-    #                                       stderr=subprocess.STDOUT,
-    #                                       universal_newlines=True)
-    #     with open(out_file, 'rt') as f:
-    #         scr_lines = f.readlines()
-
-    #     # Verify results
-    #     self.assertEqual( scr_out, '' )
-
-    #     self.assertEqual( len(scr_lines), len(exp_out) )
-    #     for exp_line in exp_out:
-    #         self.assertTrue( exp_line in scr_lines )
-
-    #     # Remove output file and try again with -o option
-    #     os.unlink( out_file )
-
-    #     # Try -o option.
-    #     scr_out = subprocess.check_output([self.script_name, 'dupe_files', 
-    #                                        '-o', out_file,
-    #                                        'rootA/TreeA', 'rootA/LeafB'], 
-    #                                       stderr=subprocess.STDOUT,
-    #                                       universal_newlines=True)
-    #     with open(out_file, 'rt') as f:
-    #         scr_lines = f.readlines()
-
-    #     # Verify results
-    #     self.assertEqual( scr_out, '' )
-
-    #     self.assertEqual( len(scr_lines), len(exp_out) )
-    #     for exp_line in exp_out:
-    #         self.assertTrue( exp_line in scr_lines )
-
-    #     # Remove output file
-    #     os.unlink( out_file )
-        
-    # def test_root_prefix(self):
-    #     """Tests dupe_files subcommand with --root-prefix option.
-    #     """
-
-    #     mod_time = int(time.time())
-    #     check_time = datetime.datetime.fromtimestamp(mod_time)
-    #     exp_out = ['Only in LeafB: BunchOfAs.txt',
-    #                'Only in LeafB: BunchOfBs.txt',
-    #                'Only in TreeA: DirA', '']
-
-    #     # Call open_db, which should create db and its tables
-    #     self.open_db( self.default_db, False )
-
-    #     # Populate the database with schema 1.
-    #     exp_data = self.get_schema_1( str(mod_time), check_time )
-    #     self.populate_db_from_tree( exp_data )
-    #     self.conn.close()
-
-    #     # Dupe_Files targets
-    #     scr_out = subprocess.check_output([self.script_name, 'dupe_files', 
-    #                                        '--root-prefix', 'rootA',
-    #                                        'TreeA', 'LeafB'], 
-    #                                       stderr=subprocess.STDOUT,
-    #                                       universal_newlines=True)
-    #     scr_lines = scr_out.split('\n')
-
-    #     # Verify results
-    #     self.assertEqual( len(scr_lines), len(exp_out) )
-    #     for exp_line in exp_out:
-    #         self.assertTrue( exp_line in scr_lines )
-        
-    # def test_use_root(self):
-    #     """Tests dupe_files subcommand with --use-root option.
-    #     """
-
-    #     mod_time = int(time.time())
-    #     check_time = datetime.datetime.fromtimestamp(mod_time)
-    #     exp_out = ['Only in my_temp/LeafB: BunchOfAs.txt',
-    #                'Only in my_temp/LeafB: BunchOfBs.txt',
-    #                'Only in my_temp/TreeA: DirA', '']
-
-    #     # Call open_db, which should create db and its tables
-    #     self.open_db( self.default_db, False )
-
-    #     # Populate the database with schema 1.
-    #     exp_data = self.get_schema_1( str(mod_time), check_time )
-    #     self.populate_db_from_tree( exp_data )
-    #     self.conn.close()
-
-    #     # Dupe_Files targets
-    #     scr_out = subprocess.check_output([self.script_name, 'dupe_files', 
-    #                                        '--use-root', 'rootA', 
-    #                                        'my_temp/TreeA', 'my_temp/LeafB'], 
-    #                                       stderr=subprocess.STDOUT,
-    #                                       universal_newlines=True)
-    #     scr_lines = scr_out.split('\n')
-
-    #     # Verify results
-    #     self.assertEqual( len(scr_lines), len(exp_out) )
-    #     for exp_line in exp_out:
-    #         self.assertTrue( exp_line in scr_lines )
-        
-    # def test_invalid_target_left(self):
-    #     """Tests dupe_files subcommand with an invalid target on left and a valid
-    #     target on right.
-    #     """
-
-    #     mod_time = int(time.time())
-    #     check_time = datetime.datetime.fromtimestamp(mod_time)
-    #     exp_out = ["'rootA/TreeB' not in database!"]
-
-    #     # Call open_db, which should create db and its tables
-    #     self.open_db( self.default_db, False )
-
-    #     # Populate the database with schema 1.
-    #     exp_data = self.get_schema_1( str(mod_time), check_time )
-    #     self.populate_db_from_tree( exp_data )
-    #     self.conn.close()
-
-    #     # Dupe_Files targets
-    #     scr_out = subprocess.check_output([self.script_name, 'dupe_files', 
-    #                                        'rootA/TreeB', 'rootA/LeafB'], 
-    #                                       stderr=subprocess.STDOUT,
-    #                                       universal_newlines=True)
-    #     tmp_scr_lines = scr_out.split('\n')
-    #     scr_lines = []
-    #     for idx in range(0, len(tmp_scr_lines)):
-    #         find_result = tmp_scr_lines[idx].find("] ")
-    #         if 0 < len(tmp_scr_lines[idx]) and 0 < find_result:
-    #             scr_lines.append( tmp_scr_lines[idx][find_result + 2:] )
-
-    #     # Verify results
-    #     self.assertEqual( len(scr_lines), len(exp_out) )
-    #     for exp_line in exp_out:
-    #         self.assertTrue( exp_line in scr_lines )
-
-    # def test_invalid_target_right(self):
-    #     """Tests dupe_files subcommand with a valid target on left and an invalid
-    #     target on right.
-    #     """
-
-    #     mod_time = int(time.time())
-    #     check_time = datetime.datetime.fromtimestamp(mod_time)
-    #     exp_out = ["'rootA/LeafA' not in database!"]
-
-    #     # Call open_db, which should create db and its tables
-    #     self.open_db( self.default_db, False )
-
-    #     # Populate the database with schema 1.
-    #     exp_data = self.get_schema_1( str(mod_time), check_time )
-    #     self.populate_db_from_tree( exp_data )
-    #     self.conn.close()
-
-    #     # Dupe_Files targets
-    #     scr_out = subprocess.check_output([self.script_name, 'dupe_files', 
-    #                                        'rootA/TreeA', 'rootA/LeafA'], 
-    #                                       stderr=subprocess.STDOUT,
-    #                                       universal_newlines=True)
-    #     tmp_scr_lines = scr_out.split('\n')
-    #     scr_lines = []
-    #     for idx in range(0, len(tmp_scr_lines)):
-    #         find_result = tmp_scr_lines[idx].find("] ")
-    #         if 0 < len(tmp_scr_lines[idx]) and 0 < find_result:
-    #             scr_lines.append( tmp_scr_lines[idx][find_result + 2:] )
-
-    #     # Verify results
-    #     self.assertEqual( len(scr_lines), len(exp_out) )
-    #     for exp_line in exp_out:
-    #         self.assertTrue( exp_line in scr_lines )
-
-    # def test_invalid_target_both(self):
-    #     """Tests dupe_files subcommand with two invalid targets.
-    #     """
-
-    #     mod_time = int(time.time())
-    #     check_time = datetime.datetime.fromtimestamp(mod_time)
-    #     exp_out = ["'rootA/TreeB' not in database!",
-    #                "'rootA/LeafA' not in database!"]
-
-    #     # Call open_db, which should create db and its tables
-    #     self.open_db( self.default_db, False )
-
-    #     # Populate the database with schema 1.
-    #     exp_data = self.get_schema_1( str(mod_time), check_time )
-    #     self.populate_db_from_tree( exp_data )
-    #     self.conn.close()
-
-    #     # Dupe_Files targets
-    #     scr_out = subprocess.check_output([self.script_name, 'dupe_files', 
-    #                                        'rootA/TreeB', 'rootA/LeafA'], 
-    #                                       stderr=subprocess.STDOUT,
-    #                                       universal_newlines=True)
-    #     tmp_scr_lines = scr_out.split('\n')
-    #     scr_lines = []
-    #     for idx in range(0, len(tmp_scr_lines)):
-    #         find_result = tmp_scr_lines[idx].find("] ")
-    #         if 0 < len(tmp_scr_lines[idx]) and 0 < find_result:
-    #             scr_lines.append( tmp_scr_lines[idx][find_result + 2:] )
-
-    #     # Verify results
-    #     self.assertEqual( len(scr_lines), len(exp_out) )
-    #     for exp_line in exp_out:
-    #         self.assertTrue( exp_line in scr_lines )
-
 # Allow unit test to run on its own
 if __name__ == '__main__':
     unittest.main()
