7d372b2934de564bece48e3fe6393b2b9f0c0ffb
Jeff Backus <jeff.backus@gmail.com>
2013-12-15 11:20:08 -0500

    * Added test_existing_subtree() to test_scan.py. Resolves issue #86.

--- unit_tests/test_scan.py
+++ unit_tests/test_scan.py
@@ -158,6 +158,56 @@ class TestScan(BrdUnitBase):
         self.assertEqual( results['right'], None )
         self.assertNotEqual( len(results['common']), 0 )
         
+    def test_existing_subtree(self):
+        """Tests scan subcommand with an existing, changed subtree.
+        """
+
+        # Call open_db, which should create db and its tables
+        self.open_db( self.default_db, False )
+
+        mod_time = datetime.datetime.fromtimestamp(int(float(time.time())))
+        check_time = mod_time
+        mod_time = mod_time - datetime.timedelta(days=30)
+
+        # Populate the database with schema 5, modified 1 month ago.
+        exp_data = self.get_schema_5( mod_time, mod_time, 'test_tree/rootA' ) 
+        self.populate_db_from_tree( exp_data )
+        self.conn.close()
+
+        # Populate filesystem with schema 1, modified recently
+        post_data = self.get_schema_1( check_time, check_time, 'rootA' )
+        self.build_tree( post_data )
+
+        # Replce rootA/TreeA in expected data with changed subtree
+        exp_data['roots']['test_tree/rootA']['children']['TreeA'] = \
+            post_data['roots']['rootA']['children']['TreeA']
+
+        # Check targets
+        scr_out = subprocess.check_output([self.script_name, 'scan',
+                                           'test_tree/rootA/TreeA'],
+                                          stderr=subprocess.STDOUT,
+                                          universal_newlines=True)
+
+        # Call open_db, which should create db and its tables
+        self.open_db( self.default_db, False )
+
+        # Get contents of database
+        got_data = self.build_tree_data_from_db( self.conn.cursor() )
+        self.conn.close()
+
+        # Remove contents and ID fields and compare
+        exp_data = self.strip_fields(exp_data, ["contents","File_ID",
+                                                "Parent_ID","Path_ID"])
+        got_data = self.strip_fields(got_data, ["contents","File_ID",
+                                                "Parent_ID","Path_ID"])
+        results = self.diff_trees( exp_data['roots']['test_tree/rootA'], 
+                                   got_data['roots']['test_tree/rootA'] )
+
+        # Verify results 
+        self.assertEqual( results['left'], None )
+        self.assertEqual( results['right'], None )
+        self.assertNotEqual( len(results['common']), 0 )
+        
 # Allow unit test to run on its own
 if __name__ == '__main__':
     unittest.main()
